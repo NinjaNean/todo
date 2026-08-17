fc98794066660aba0929c14dcb5ddcf97362bf99
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2026-08-17 20:00:18 +0200

    - Added React icons. - HTML and CSS done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,76 @@
 import { useState } from "react";
 import "./App.css";
+import { MdCalendarMonth, MdOutlineCheck, MdOutlineAdd } from "react-icons/md";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const todo = [
+  const [tasks, setTask] = useState([
     {
       task: "Take out the trash",
       date: "Aug 16",
+      border: "--pink",
       completed: false,
     },
     {
       task: "Do the laundry",
       date: "Aug 16",
+      border: "--orange",
       completed: false,
     },
     {
       task: "Clean the kitchen",
       date: "Aug 16",
+      border: "--green",
       completed: true,
     },
-  ];
+  ]);
 
   return (
     <div className="container">
       <section className="task-container">
         <div>
           <h1>Daily Task</h1>
-          <p>2/5 Task Completed</p>
+          <p className="task-completed-text">2/5 Task Completed</p>
         </div>
         <div>
-          <p>
+          <p className="bar-text">
             You are almost done go ahead <span>40%</span>
           </p>
-          <div className="bar">BAR PLACEHOLDER</div>
+          <div className="bar">
+            <div style={{ width: `40%` }}></div>
+          </div>
         </div>
       </section>
 
       <section className="add-task-container">
         <input type="text" placeholder="Add a new task..." />
-        <button>+</button>
+        <button className="flex-center">
+          <MdOutlineAdd fill="black" />
+        </button>
       </section>
 
       <section className="task-state">
-        <button>Active (3)</button>
+        <button className="active-tab">Active (3)</button>
         <button>Completed (2)</button>
       </section>
 
       <section className="task-list">
         <h2>Todays task</h2>
         <ul>
-          {todo.map((item, index) => (
-            <li key={index}>
+          {tasks.map((item, index) => (
+            <li
+              key={index}
+              className="task"
+              style={{ borderColor: `var(${item.border})` }}
+            >
               <div>
                 <p>{item.task}</p>
-                <p>{item.date}</p>
+                <p className="added-date">
+                  <MdCalendarMonth /> {item.date}
+                </p>
               </div>
-              <button>✓</button>
+              <button className="done-task flex-center">
+                <MdOutlineCheck fill="#ea7c69" />
+              </button>
             </li>
           ))}
         </ul>
