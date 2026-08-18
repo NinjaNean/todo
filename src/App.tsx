547670c9b3ae69f0ed38bb19d098624620d594ae
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2026-08-18 19:25:48 +0200

    Refactor task management and enhance UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,40 @@
 import { useState } from "react";
 import "./App.css";
-import {
-  MdCalendarMonth,
-  MdOutlineCheck,
-  MdOutlineAdd,
-  MdOutlineDelete,
-} from "react-icons/md";
+import { MdOutlineCalendarMonth } from "react-icons/md";
+import { BsCheck2Circle, BsTrash, BsPlusLg, BsCheckLg } from "react-icons/bs";
 
 function App() {
-  const [tasks, setTask] = useState([
+  const [tasks, setTasks] = useState([
     {
-      id: 1,
+      id: "3b241101-e2bb-4255-8caf-4136c566a964",
       task: "Take out the trash",
       date: "Aug 16",
       border: "--pink",
       completed: false,
     },
     {
-      id: 2,
+      id: "6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b",
       task: "Do the laundry",
       date: "Aug 16",
       border: "--orange",
       completed: false,
     },
     {
-      id: 3,
+      id: "9c3c1e2b-7b08-410a-8bf8-21d1b9136c53",
       task: "Clean the kitchen",
       date: "Aug 16",
       border: "--green",
       completed: false,
     },
     {
-      id: 4,
+      id: "8c772c68-085e-4a61-9c60-f472851d7c37",
       task: "Take a walk",
       date: "Aug 16",
       border: "--purple",
       completed: true,
     },
     {
-      id: 5,
+      id: "10b7543c-62ae-4c7b-839c-46a4897f1f97",
       task: "Vacum the house",
       date: "Aug 16",
       border: "--blue",
@@ -46,10 +42,20 @@ function App() {
     },
   ]);
 
-  let procent =
-    (tasks.filter((task) => task.completed).length / tasks.length) * 100;
+  const borderColors = ["--green", "--blue", "--pink", "--orange", "--purple"];
 
   const [activeTab, setActiveTab] = useState(false);
+
+  const [addTask, setAddTask] = useState("");
+
+  const procent = Math.round(
+    (tasks.filter((task) => task.completed).length / tasks.length) * 100,
+  );
+
+  const today = new Date().toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
 
   function handleTask(id: string | number) {
     let task = tasks.find((task) => task.id === id);
@@ -57,18 +63,37 @@ function App() {
     if (task) {
       let newTodo = tasks.filter((todo) => todo.id !== task.id);
       task.completed = !task.completed;
-      setTask([...newTodo, task]);
+      setTasks([...newTodo, task]);
     } else {
       return;
     }
   }
 
-  function deleteTask(id: string | number) {
+  function deleteTask(id: string) {
     let task = tasks.find((task) => task.id === id);
 
     if (task) {
       let newTodo = tasks.filter((todo) => todo.id !== task.id);
-      setTask(newTodo);
+      setTasks(newTodo);
+    } else {
+      return;
+    }
+  }
+
+  function addNewTask() {
+    if (addTask !== "") {
+      let id = crypto.randomUUID();
+      setTasks([
+        ...tasks,
+        {
+          id: id,
+          completed: false,
+          task: addTask.charAt(0).toUpperCase() + addTask.slice(1),
+          date: today,
+          border: borderColors[Math.floor(Math.random() * borderColors.length)],
+        },
+      ]);
+      setAddTask("");
     } else {
       return;
     }
@@ -96,9 +121,15 @@ function App() {
       </section>
 
       <section className="add-task-container">
-        <input type="text" placeholder="Add a new task..." />
-        <button className="flex-center">
-          <MdOutlineAdd fill="black" />
+        <input
+          value={addTask}
+          onChange={(e) => setAddTask(e.target.value)}
+          type="text"
+          placeholder="Add a new task..."
+          onKeyDown={(e) => e.key === "Enter" && addNewTask()}
+        />
+        <button onClick={() => addNewTask()} className="flex-center">
+          <BsPlusLg fill="black" />
         </button>
       </section>
 
@@ -117,40 +148,52 @@ function App() {
         </button>
       </section>
 
-      <section className="task-list">
+      <section>
         <h2>{activeTab ? "Completed" : "Todays task"}</h2>
-        <ul className={activeTab ? "completed" : ""}>
-          {tasks
-            .filter((task) => task.completed === activeTab)
-            .map((item) => (
-              <li
-                key={item.id}
-                className="task"
-                style={{ borderColor: `var(${item.border})` }}
-              >
-                <div>
-                  <p className="task-text">{item.task}</p>
-                  <p className="added-date">
-                    <MdCalendarMonth /> {item.date}
-                  </p>
-                </div>
-                <div className="task-buttons ">
-                  <button
-                    onClick={() => deleteTask(item.id)}
-                    className="delete-task-button flex-center"
-                  >
-                    <MdOutlineDelete />
-                  </button>
-                  <button
-                    onClick={() => handleTask(item.id)}
-                    className="done-task-button flex-center"
-                  >
-                    <MdOutlineCheck fill={activeTab ? "" : "#ea7c69"} />
-                  </button>
-                </div>
-              </li>
-            ))}
-        </ul>
+        {tasks.length === 0 ? (
+          <div className="no-task">
+            <div className="check-mark flex-center">
+              <BsCheck2Circle size={`5rem`} fill="#ea7c69" />
+            </div>
+            <h3>No tasks left!</h3>
+            <p className="create-new-task-text">
+              Create a new task to get started
+            </p>
+          </div>
+        ) : (
+          <ul className={activeTab ? "completed" : ""}>
+            {tasks
+              .filter((task) => task.completed === activeTab)
+              .map((item) => (
+                <li
+                  key={item.id}
+                  className="task"
+                  style={{ borderColor: `var(${item.border})` }}
+                >
+                  <div>
+                    <p className="task-text">{item.task}</p>
+                    <p className="added-date">
+                      <MdOutlineCalendarMonth /> {item.date}
+                    </p>
+                  </div>
+                  <div className="task-buttons ">
+                    <button
+                      onClick={() => deleteTask(item.id)}
+                      className="delete-task-button flex-center"
+                    >
+                      <BsTrash />
+                    </button>
+                    <button
+                      onClick={() => handleTask(item.id)}
+                      className="done-task-button flex-center"
+                    >
+                      <BsCheckLg fill={activeTab ? "" : "#ea7c69"} />
+                    </button>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        )}
       </section>
     </div>
   );
