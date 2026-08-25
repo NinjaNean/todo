9b9e570aa129dd560924f052e06109e82b536f3b
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2026-08-25 15:44:12 +0200

    Add Calender component with date selection and week display

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./Calender.css";
 
 function Calender() {
   const [startDate, setStartDate] = useState(new Date());
+  const week = ["M", "T", "W", "T", "F", "S", "S"];
 
   return (
-    <>
+    <div className="date-container">
       <DatePicker
         selected={startDate}
         onChange={(date) => setStartDate(date)}
+        dateFormat="MMMM, yyyy"
+        className="month"
+        showIcon
       />
-    </>
+
+      <div className="week-carousel">
+        {week.map((w, index) => {
+          return (
+            <div className="day" key={index}>
+              <p>{w}</p>
+              <p>17</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
