9952f718e8d39d66b891dfb5833f7e7cd464bd59
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2026-08-25 16:05:41 +0200

    Refactor Calender component to enhance week display and improve date handling

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -2,10 +2,19 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Calender.css";
+import { addDays, format, isSameDay, startOfWeek } from "date-fns";
 
 function Calender() {
   const [startDate, setStartDate] = useState(new Date());
-  const week = ["M", "T", "W", "T", "F", "S", "S"];
+  const startOfCurrentWeek = startOfWeek(startDate, { weekStartsOn: 1 });
+  const weekDays = Array.from({ length: 7 }).map((_, index) => {
+    const dayDate = addDays(startOfCurrentWeek, index);
+    return {
+      dayName: format(dayDate, "EEEEE"), // M, T, O...
+      dayNumber: format(dayDate, "d"), // 25
+      date: dayDate,
+    };
+  });
 
   return (
     <div className="date-container">
@@ -18,11 +27,13 @@ function Calender() {
       />
 
       <div className="week-carousel">
-        {week.map((w, index) => {
+        {weekDays.map((w, index) => {
+          const isSelected = isSameDay(w.date, startDate);
+
           return (
-            <div className="day" key={index}>
-              <p>{w}</p>
-              <p>17</p>
+            <div className={`day ${isSelected ? "active" : ""}`} key={index}>
+              <p>{w.dayName}</p>
+              <p>{w.dayNumber}</p>
             </div>
           );
         })}
